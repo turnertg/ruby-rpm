da73d2637c8cec6fbd330f7de9add8d9260b584c
koshigoe <koshigoeb@gmail.com>
2018-10-26 08:05:58 +0900

    feat: add shell script for building RPM files.

---
+++ .circleci/build-rpm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xeu
+
+RUBY_X_Y_VERSION=$1
+SPEC_FILE=ruby-${RUBY_X_Y_VERSION}.spec
+RUBY_X_Y_Z_VERSION=$(grep '^Version: ' ~/ruby-rpm/${SPEC_FILE} | awk '{ print $NF }')
+
+cp ~/ruby-rpm/${SPEC_FILE} ~/rpmbuild/SPECS/
+
+cd ~/rpmbuild/SOURCES
+curl -LO https://cache.ruby-lang.org/pub/ruby/${RUBY_X_Y_VERSION}/ruby-${RUBY_X_Y_Z_VERSION}.tar.gz
+
+rpmbuild -ba ~/rpmbuild/SPECS/${SPEC_FILE}
+
+DEST_DIR=/tmp/ruby-${RUBY_X_Y_VERSION}-rpm
+mkdir -p ${DEST_DIR}
+cp ~/rpmbuild/RPMS/x86_64/* ${DEST_DIR}
+cp ~/rpmbuild/SRPMS/* ${DEST_DIR}
